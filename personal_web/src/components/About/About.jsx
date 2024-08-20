606e2e77ec790e185b9c2d95c134f4d18d644ee7
Claire Wanjia Tang <113147894+tangwanjia@users.noreply.github.com>
2024-08-19 21:42:27 -0400

    skills&hovers

--- personal_web/src/components/About/About.jsx
+++ personal_web/src/components/About/About.jsx
@@ -4,7 +4,7 @@ import ocean from "./../../assets/ocean.jpg";
 
 function About() {
   return (
-    <div className={"${aboutCSS.AboutWrapper} section"}>
+    <div className={`${aboutCSS.AboutWrapper} section`}>
       <div className={aboutCSS.aboutContainer}>
         <h2>About Me</h2>
         <p>
@@ -13,31 +13,47 @@ function About() {
           looking for a position in website designer and various computer
           languages, good in js, react, network and database
         </p>
-      </div>
-      <div className={aboutCSS.skilWrapper}>
-        <div className={aboutCSS.skil}>
-          <h3>JavaScript</h3>
-          <div className={aboutCSS.line}></div>
-          <h3>HTML</h3>
-          <div className={aboutCSS.line}></div>
-          <h3>React</h3>
-          <div className={aboutCSS.line}></div>
-          <h3>Figma</h3>
-          <div className={aboutCSS.line}></div>
-          <h3>Network</h3>
-          <div className={aboutCSS.line}></div>
-          <h3>PhotoShop</h3>
-          <div className={aboutCSS.line}></div>
-          <h3>Illustrator</h3>
-          <div className={aboutCSS.line}></div>
-          <h3>Premiere</h3>
-          <div className={aboutCSS.line}></div>
+
+        <div className={aboutCSS.skils}>
+          <h2>My Skills</h2>
+          <p>xxxxxx</p>
+          <div className={aboutCSS.skilWrapper}>
+            <div className={aboutCSS.skil}>
+              <h3>JavaScript</h3>
+              <div className={aboutCSS.line}></div>
+            </div>
+            <div className={aboutCSS.skil}>
+              <h3>HTML & CSS</h3>
+              <div className={aboutCSS.line}></div>
+            </div>
+            <div className={aboutCSS.skil}>
+              <h3>React</h3>
+              <div className={aboutCSS.line}></div>
+            </div>
+            <div className={aboutCSS.skil}>
+              <h3>Figma</h3>
+              <div className={aboutCSS.line}></div>
+            </div>
+
+            <div className={aboutCSS.skil}>
+              <h3>PhotoShop</h3>
+              <div className={aboutCSS.line}></div>
+            </div>
+            <div className={aboutCSS.skil}>
+              <h3>Illustrator</h3>
+              <div className={aboutCSS.line}></div>
+            </div>
+            <div className={aboutCSS.skil}>
+              <h3>Premier</h3>
+              <div className={aboutCSS.line}></div>
+            </div>
+          </div>
         </div>
       </div>
 
       <div className={aboutCSS.AboutImg}>
         <img src={ocean} alt="ocean" />
-        <div className={aboutCSS.Intro}>
+        <div className={aboutCSS.Exp}>
           <h2>1+ of Experience </h2>
         </div>
       </div>
