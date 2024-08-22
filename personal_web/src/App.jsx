bd853ad81a99299ae5e7f25000d77f49e6b9fe50
Claire Wanjia Tang <113147894+tangwanjia@users.noreply.github.com>
2024-08-21 20:24:30 -0400

    ser page

--- personal_web/src/App.jsx
+++ personal_web/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav";
+import Serv from "./components/Serv/Serv";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Header />
       <About />
+      <Serv />
     </>
   );
 }
---
+++ personal_web/src/components/Serv/Serv.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import serviceCSS from "./../Serv/Serv.module.css";
+import { Tilt } from "react-tilt";
+// import {Tilt} from "react-parallax-tilt";
+
+function Service() {
+  return (
+    <div className={`${serviceCSS.ServiceWrapper} section`}>
+      <h2>What I do</h2>
+      <p className={serviceCSS.pera}>xxxxxxxxxx</p>
+
+      <div className={serviceCSS.ServiceCards}>
+        <Tilt>
+          <div className={serviceCSS.ServiceCard}>
+            <i className="ri-database-line"></i>
+            <h3> Web Development </h3>
+            <p>XXXX</p>
+            <h4>
+              Lean Morn <i className="ri-arrow-right-line"></i>
+            </h4>
+          </div>
+        </Tilt>
+        <div className={serviceCSS.ServiceCard}>
+          <i className="ri-pantone-line"></i>
+          <h3> UI/UX designer </h3>
+          <p>XXXX</p>
+          <h4>
+            Lean Morn <i className="ri-arrow-right-line"></i>
+          </h4>
+        </div>
+        <div className={serviceCSS.ServiceCard}>
+          <i className="ri-smartphone-line"></i>
+          <h3> App Development </h3>
+          <p>XXXX</p>
+          <h4>
+            Lean Morn <i className="ri-arrow-right-line"></i>
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
